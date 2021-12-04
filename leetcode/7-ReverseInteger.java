c74dc2aca114b1105640475b5ef3899972baf07f
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2021-12-04 20:17:27 -0300

    feat: revers integer

---
+++ leetcode/7-ReverseInteger.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long rev = 0L;
+        while(x != 0) {
+            rev = rev*10 + x%10;
+            if (rev >= Integer.MAX_VALUE || rev <= Integer.MIN_VALUE) {
+                return 0;
+            }
+            x /= 10;
+        }
+        return (int)(rev * ((x < 0) ? -1 : 1));
+    }
+}
