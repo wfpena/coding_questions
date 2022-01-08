f831b5b109b16885e25901146904d5e3f0002179
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-01-08 13:34:53 -0300

    feat unique paths

---
+++ leetcode/62-UniquePaths.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        return helper(m, n, dp);
+    }
+    
+    private int helper(int m, int n, int[][] dp) {
+        if (m == 1 && n == 1) return 1;
+        if (m <= 0 || n <= 0) return 0;
+        if (dp[m-1][n-1] != 0) return dp[m-1][n-1];
+        dp[m-1][n-1] = helper(m - 1, n, dp) + helper(m, n-1, dp);
+        return dp[m-1][n-1];
+    }
+}
