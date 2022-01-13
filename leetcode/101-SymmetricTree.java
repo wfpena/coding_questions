094af1dbef37ae1a1ed272c6ed9135a574145365
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-01-13 08:01:29 -0300

    feat: symmetric tree

---
+++ leetcode/101-SymmetricTree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return checkMirror(root.left, root.right);
+    }
+    
+    private boolean checkMirror(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) return true;
+        if (n1 == null || n2 == null) return false;
+        return (n1.val == n2.val) && checkMirror(n1.left, n2.right) && checkMirror(n1.right, n2.left);
+    }
+    
+}
