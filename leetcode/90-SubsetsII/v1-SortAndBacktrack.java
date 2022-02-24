41b49a2aeaf0f1c8ab90d9a60e17dea5928d8ef5
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-02-24 06:52:12 -0300

    feat: subsets II sorting and backtrack

---
+++ leetcode/90-SubsetsII/v1-SortAndBacktrack.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, new ArrayList<>(), result, new boolean[nums.length], 0);
+        return result;
+    }
+    
+    private void backtrack(int[] nums, List<Integer> tmp, List<List<Integer>> result, boolean[] used, int idx) {
+        result.add(new ArrayList<>(tmp));
+        Set<Integer> set = new HashSet<>();
+        for (int i = idx; i < nums.length; i++) {
+            if (set.contains(nums[i]) || used[i]) continue;
+            set.add(nums[i]);
+            used[i] = true;
+            tmp.add(nums[i]);
+            backtrack(nums, tmp, result, used, i+1);
+            tmp.remove(tmp.size()-1);
+            used[i] = false;
+        }
+    }
+}
