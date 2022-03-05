939222ec5fc582cd67b5b54c9d822feddca695af
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-03-05 08:48:26 -0300

    feat: populating next right pointer for each node

---
+++ leetcode/116-PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return root;
+        Node node  = root;
+        Node nextLevel = null;
+        while (node.left != null) {
+            nextLevel = node.left;
+            // System.out.println(node.val);
+            while (node != null) {
+                node.left.next = node.right;
+                if (node.next != null) {
+                    node.right.next = node.next.left;
+                }
+                node = node.next;
+            }
+            node = nextLevel;
+        }
+        return root;
+    }
+}
