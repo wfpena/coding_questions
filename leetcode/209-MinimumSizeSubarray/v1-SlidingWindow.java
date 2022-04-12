bf085b807357fb30a3c18ca74b0190541ee74a3e
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-04-12 06:25:11 -0300

    feat: minimum size subarray

---
+++ leetcode/209-MinimumSizeSubarray/v1-SlidingWindow.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int right = 0;
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int curSum = 0;
+        while (right < nums.length) {
+            curSum += nums[right++];
+            while (curSum >= target) {
+                minLen = Math.min(minLen, right-left);
+                if (minLen == 1) return minLen;
+                curSum -= nums[left++];
+            }
+            
+        }
+        if (minLen == Integer.MAX_VALUE) return 0;
+        return minLen;
+    }
+}
