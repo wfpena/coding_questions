ee69fff3a7c4f13e053b8655fca0bd45848ebb98
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-04-20 06:15:07 -0300

    feat: find median in data stream using two priority queues

---
+++ leetcode/295-FindMedianFromDataStream/v2-TwoPriorityQueues.java
@@ -0,0 +1,44 @@
+class MedianFinder {
+    
+    Queue<Integer> left;
+    Queue<Integer> right;
+    
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        left = new PriorityQueue<>(Collections.reverseOrder());
+        right = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        // if (left.isEmpty()) {
+        //     left.add(num);
+        //     return;
+        // }
+        left.add(num);
+        if (left.size() > right.size()) {
+            right.add(left.poll());
+        } else if (!right.isEmpty() && right.peek() <= num) {
+            left.poll();
+            left.add(right.poll());
+            right.add(num);
+        }
+        // if (!right.isEmpty() && !left.isEmpty()) {
+        //     System.out.println(num + " left " + left.peek() + " right: " + right.peek());
+        // }
+        
+    }
+    
+    public double findMedian() {
+        int count = right.size() + left.size();
+        if (count == 0) return 0;
+        if (count % 2 == 0) return (right.peek()+left.peek())/2.0;
+        return right.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
