015a1b3adae32e37bd203dcac9d2a743ed016ea6
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-04-23 08:44:11 -0300

    feat: find median in data stream simplified logic but more time

---
+++ leetcode/295-FindMedianFromDataStream/v5-SimplifiedLogicButTakesMoreTimeThanPrevious.java
@@ -0,0 +1,39 @@
+class MedianFinder {
+    
+    Queue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+    Queue<Integer> right = new PriorityQueue<>();
+    
+    /** initialize your data structure here. */
+    public MedianFinder() {
+    }
+    
+    public void addNum(int num) {
+        if (left.isEmpty()) {
+            left.add(num);
+            return;
+        }
+        if (left.peek() > num) {
+            left.add(num);
+        } else {
+            right.add(num);
+        }
+        
+        if (left.size() > right.size() + 1) {
+            right.add(left.poll());
+        } else if (right.size() > left.size()) {
+            left.add(right.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (right.size() == left.size()) return (right.peek()+left.peek())/2.0;
+        return left.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
