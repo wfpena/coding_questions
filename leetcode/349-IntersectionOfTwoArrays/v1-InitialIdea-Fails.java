96c3eeb9ff973da97dc24ba4c8dc117d21de62d3
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-05-11 08:19:05 -0300

    feat: intersection of two arrays initial idea

---
+++ leetcode/349-IntersectionOfTwoArrays/v1-InitialIdea-Fails.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1.length < nums2.length) return intersection(nums2, nums1);
+        Set<Integer> set = new HashSet<>();
+        int i =0;
+        while(i < nums1.length) {
+            if(binarySearch(nums2, nums1[i])) {
+                set.add(nums1[i]);
+            }
+            i++;
+        }
+        int[] res = new int[set.size()];
+        int k = 0;
+        Integer[] arr = set.stream().toArray(Integer[]::new);
+        for(Integer j : arr) {
+            res[k++] = j;
+        }
+        return res;
+    }
+    
+    private boolean binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<end) {
+            int mid = (start+end)/2;
+            if(nums[mid]==target) return true;
+            if (nums[mid]>target) {
+                end=mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+}
