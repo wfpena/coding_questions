58358bb5f99876254585527c5f8b3b8b657bdc1f
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-05-21 11:25:12 -0300

    feat: kth smallest element in a sorted matrix nested loop improved

---
+++ leetcode/378-KthSmallestElementInASortedMatrix/v4-WithImprovements.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        if (k == 1) return matrix[0][0];
+        int n = matrix.length;
+        if (k == n*n) return matrix[n-1][n-1];
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int j = (int) Math.ceil((double)(k-1)/n);
+        // System.out.println(j);
+        int size = ((j+1)*j)/2;
+        while (j+1 < 2*n) {
+            int col = j;
+            int lin = 0;
+            if (j+1 >= n) {
+                col = n-1;
+                lin = j+1-n;
+            }
+            int limit = col;
+            int min = lin*col + lin + col + 1;
+            int max = (lin*n)+(col*n)-(lin*col)+1;
+            if (min > k) break;
+            while (lin <= limit) {
+                // System.out.println(min + " " + max);
+                pq.add(matrix[lin][col]);
+                // if (min <= k && max >= k) {
+                //     pq.add(matrix[lin][col]);
+                // }
+                col--;
+                lin++;
+                min = lin*col + lin + col + 1;
+                max = (lin*n)+(col*n)-(lin*col)+1;
+            }
+            j++;
+        }
+        while (pq.size() + size > k) {
+            // System.out.println("poll: " + (pq.size() + size) + " " + pq.peek());
+            pq.poll();
+        }
+        // System.out.println("Out: " + pq.size() + " size:" + size + " " + pq.peek());
+        return pq.poll();
+    }
+    
+    private void throughDiagonal(int j, int i, int[][] matrix, Queue<Integer> pq) {
+        int limit = j;
+        while (i <= limit) {
+            pq.add(matrix[i][j]);
+            j--;
+            i++;
+        }
+    }
+}
