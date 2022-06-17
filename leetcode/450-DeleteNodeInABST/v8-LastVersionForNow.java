0e63f50d96dae0f8c433a2deea72a3f175e2974c
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-06-17 06:56:15 -0300

    feat: delete node in a binary search tree last version for now

---
+++ leetcode/450-DeleteNodeInABST/v8-LastVersionForNow.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+        if (root.val == key) {
+            if (root.right == null) return root.left;
+            TreeNode leftmostRight = root.right;
+            while (leftmostRight.left != null) {
+                leftmostRight = leftmostRight.left;
+            }
+            leftmostRight.left = root.left;
+            return root.right;
+        }
+        if (root.val > key) root.left = deleteNode(root.left, key);
+        else root.right = deleteNode(root.right, key);
+        return root;
+    }
+}
