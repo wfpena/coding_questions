ac64d87947fe4243ed8576035abfd38e7c1df3d9
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-07-20 07:02:52 -0300

    feat: sum of square numbers a bit worst time

---
+++ leetcode/633-SumOfSquareNumbers/v8-BitWorstTime.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (int a = 0; a*a<=c/2; a++) {
+            double b = Math.sqrt(c-(a*a));
+            if (b == (int)b) return true;
+        }
+        return false;
+    }
+}
