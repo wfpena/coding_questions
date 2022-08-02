9d8d9e6da40398bed6c629081da49adf69569c70
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2022-08-02 06:35:56 -0300

    feat: max length of repeated subarray using dp and nested iteration

---
+++ leetcode/718-MaximumLengthOfRepeatedSubarray/v2-UsingNestedIteration.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums2.length+1][nums1.length+1];
+        int max = 0;
+        for (int i = 1; i <= nums2.length; i++) {
+            for (int j = 1; j <= nums1.length; j++) {
+                if (nums2[i-1] == nums1[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max;
+    }
+}
